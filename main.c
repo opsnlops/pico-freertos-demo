c4f4cdc1d8e8f77372d57e0bebc7a72e770a6276
April White <april@opsnlops.io>
2022-11-20 17:19:41 -0800

    The most simple FreeRTOS on Pico project ever!

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <sys/cdefs.h>
+#include "pico/stdlib.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+_Noreturn void vBlinkTask() {
+
+    for (;;) {
+
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+
+        vTaskDelay(250);
+
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+
+        vTaskDelay(250);
+
+    }
+
+}
+
+int main() {
+
+    gpio_init(PICO_DEFAULT_LED_PIN);
+
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+    xTaskCreate(vBlinkTask, "Blink Task", 128, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+
+}
